51ca2db619e1136c31ab8d040267c45648bedbe1
Jorge Miguel Cordeiro <156421211+jorgemiguelcordeiro@users.noreply.github.com>
2025-05-24 12:27:12 +0100

    Update pipeline_registry.py

--- src/mlflow_in_claim_injury_prediction/pipeline_registry.py
+++ src/mlflow_in_claim_injury_prediction/pipeline_registry.py
@@ -1,16 +1,22 @@
 """Project pipelines."""
 
+from typing import Dict
 from kedro.pipeline import Pipeline
-from .pipelines import (
-    data_units_test,
-)
+from kedro.framework.project import find_pipelines
 
+# Import pipelines
+from .pipelines import data_units_test, feature_store
 
-def register_pipelines() -> dict[str, Pipeline]:
+def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
 
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
-    data_units_test_pipeline= data_units_test.create_pipeline()
-    return  {"data_units_tests": data_units_test_pipeline}
+    pipelines = find_pipelines()
+
+    # Add individual pipelines
+    pipelines["data_units_tests"] = data_units_test.create_pipeline()
+    pipelines["feature_store"] = feature_store.create_pipeline()
+
+    return pipelines
